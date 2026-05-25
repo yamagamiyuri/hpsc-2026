7127198b499cecef481f8b898403abc93b9715d6
yamagamiyuri <yamagamiyuri3124@gmail.com>
2026-05-25 12:57:59 +0900

    Update 17_phdf5_read.cpp

--- 09_io/17_phdf5_read.cpp
+++ 09_io/17_phdf5_read.cpp
@@ -22,12 +22,14 @@ int main (int argc, char** argv) {
   H5Sget_simple_extent_dims(globalspace, N, NULL);
   hsize_t NX = N[0], NY = N[1];
   hsize_t Nlocal[2] = {NX/dim[0], NY/dim[1]};
+  hsize_t block[2] = {NX/4, NY/4};
   hsize_t offset[2] = {mpirank / dim[0], mpirank % dim[0]};
-  for(int i=0; i<2; i++) offset[i] *= Nlocal[i];
-  hsize_t count[2] = {1,1};
-  hsize_t stride[2] = {1,1};
+  for(int i=0; i<2; i++) offset[i] *= block[i];
+  hsize_t count[2] = {2,2};
+  hsize_t stride[2] = {2,2};
+  for(int i=0; i<2; i++) stride[i] *= block[i];
   hid_t localspace = H5Screate_simple(2, Nlocal, NULL);
-  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, Nlocal);
+  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, block);
   H5Pclose(plist);
   vector<int> buffer(Nlocal[0]*Nlocal[1]);
   plist = H5Pcreate(H5P_DATASET_XFER);
